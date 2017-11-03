7deef187bf513c12ebbebaeb65713f8bdc1fec42
qiaomu <oyf@gongfuge.net>
2017-11-03 17:27:39 +0800

    add folder supported

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,96 +6,95 @@
  * @license MIT
  */
 
-var logApp = angular.module('logApp', ['luegg.directives', 'ngResource']);
+var logApp = angular.module('logApp', ['luegg.directives', 'ngResource', 'angularTreeview']);
 
-logApp.factory('LogsService', ['$resource',
-  function($resource){
-
-    return $resource('/logs.json', {}, {
-      query: {method:'GET', isArray:'true'}
-    });
-  }
-]);
+logApp.factory('FoldersService', ['$resource', function ($resource) {
+  return $resource('/folders', {}, {
+    query: { method: 'GET', isArray: false },
+    sub: { method: 'GET', isArray: false, url: '/folders/:index/:path' }
+  });
+}])
 
 logApp.factory('socket', function ($rootScope) {
   return {
-    giveSocket: function() {
-        var disconnecting = false;
-        var socket = {};
-        return {
-          connect: function(logId){
-            disconnecting = false;
-            socket = io.connect("/" + logId);
-          },
-          on: function(eventName, callback){
-            socket.on(eventName, function(){
-              var args = arguments;
-              if(!disconnecting){
-                $rootScope.$apply(function(){
-                  callback.apply(socket, args);
-                });
+    giveSocket: function () {
+      var disconnecting = false;
+      var socket = {};
+      return {
+        connect: function () {
+          disconnecting = false;
+          socket = io.connect("/view");
+        },
+        on: function (eventName, callback) {
+          socket.on(eventName, function () {
+            var args = arguments;
+            if (!disconnecting) {
+              $rootScope.$apply(function () {
+                callback.apply(socket, args);
+              });
+            }
+          });
+        },
+        emit: function (eventName, data, callback) {
+          socket.emit(eventName, data, function () {
+            var args = arguments;
+            $rootScope.$apply(function () {
+              if (callback) {
+                callback.apply(socket, args);
               }
             });
-          },
-          emit: function (eventName, data, callback) {
-            socket.emit(eventName, data, function(){
-              var args = arguments;
-              $rootScope.$apply(function(){
-                if (callback) {
-                  callback.apply(socket, args);
-                }
-              });
-            })
-          },
-          disconnect: function(){
-            disconnecting = true;
-            socket.disconnect();
-          },
-          socket: socket
-        };
+          })
+        },
+        disconnect: function () {
+          disconnecting = true;
+          socket.disconnect();
+        },
+        socket: socket
+      };
     }
   }
 });
 
+logApp.controller('logAppCtrl', function (FoldersService, socket, $scope) {
 
-logApp.controller('logAppCtrl', function(LogsService, socket, $scope){
+  $scope.query = "";
+  $scope.msg = [];
+  $scope.selectedLog = -1;
+  $scope.glued = true;
 
-    $scope.query = "";
-    $scope.msg = [];
-    $scope.selectedLog = -1;
-    $scope.glued = true;
-    $scope.logs = [];
+  $scope.folders = []
 
-    LogsService.query({}, function(data) {
-      $scope.logs = data;
-    });
-
-    $scope.sock = null;
-
-    $scope.changeLog = function(log) {
-      if ($scope.sock != null) {
-        $scope.sock.disconnect();
-
-        $scope.msg = [];
-        $scope.logs.forEach(function(log) {
-          log.css = '';
-        });
-      }
-      $scope.selectedLog = log.id;
-      log.css = 'active';
-
-      $scope.sock = socket.giveSocket();
-      $scope.sock.connect($scope.selectedLog);
-
-      $scope.sock.on('Log', function(result) {
-        $scope.msg.push(result);
-        if($scope.msg.length > 300) {
-            $scope.msg.shift();
-        }
-      });
-
-      $scope.sock.emit('init');
-
+  FoldersService.query({}, function (data) {
+    if (data.success) {
+      var folders = data.folders
+      folders.forEach(function (folder) {
+        $scope.folders.push(folder)
+      })
     }
+  });
 
+  $scope.path = ''
+
+  $scope.$watch('files.currentNode', function (node) {
+    if (!node) {
+      $scope.path = null;
+      return;
+    }
+    if ($scope.sock != null) {
+      $scope.sock.disconnect();
+      $scope.msg = [];
+    }
+    $scope.sock = socket.giveSocket();
+    $scope.sock.connect('/view');
+    $scope.sock.on('Log', function (result) {
+      $scope.msg.push(result);
+      if ($scope.msg.length > 300) {
+        $scope.msg.shift();
+      }
+    });
+    $scope.sock.emit('init', node.path);
+    $scope.path = node.path
+  });
+
+  $scope.sock = null;
 });
--- server.js
+++ server.js
@@ -7,97 +7,149 @@
  * @license MIT
  */
 
-var express = require('express')
-    , http = require('http');
+var express = require('express'),
+	http = require('http'),
+	path = require('path');
 var fs = require('fs'); // file system module
 var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
+var file = require('file');
 
 var tail = [];
 
 var logModule = require('./conf.js');
-var logfiles = logModule.logfiles;
+var logfolders = logModule.logfolders;
 var highlight = logModule.highlight;
+
+highlight.forEach(function (h) {
+	h.text = h.text.toLowerCase();
+});
 
 Tail = require('tail').Tail;
 
-app.configure(function() {
-    app.use(express.static(__dirname + '/app'));
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: false }));
+app.configure(function () {
+	app.use(express.static(__dirname + '/app'));
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: false }));
 });
 
 server.listen(7000);
 
-app.get('/logs.json', function(req, res){
-  res.send(logfiles);
+var walk = function (dir, parent) {
+	var results = []
+	var list = fs.readdirSync(dir)
+	list.forEach(function (file) {
+		var sub = path.join(parent, file)
+		var subFolder = path.join(dir, file)
+		var stat = fs.statSync(subFolder)
+
+		var obj = {
+			name: file,
+			path: new Buffer(sub).toString('base64')
+		}
+		if (stat && stat.isDirectory()) {
+			obj.children = walk(subFolder, sub)
+		}
+		results.push(obj)
+	})
+	return results
+}
+
+app.get('/folders', function (req, res) {
+	res.send({
+		folders: logfolders.map(function (f) {
+			return {
+				name: f.name,
+				path: 'Lw==',
+				children: walk(f.path, 'gongfuge')
+			}
+		}),
+		success: true
+	});
 });
 
-function getCss(line) {
+var getPath = function (relativePath) {
+	if (relativePath) {
+		relativePath = new Buffer(relativePath, 'base64').toString('ascii');
+		for (var index = 0; index < logfolders.length; index++) {
+			var folder = logfolders[index];
+			if (relativePath.indexOf(folder.name) === 0) {
+				var filename = path.join(folder.path, relativePath.replace(folder.name, ''))
+				if (fs.existsSync(filename)) return filename;
+			}
+		}
+	}
+	return null;
+};
+
+var getCss = function (line) {
 	var css = ""
-	highlight.forEach(function (elt){
-		if ( (css == "") && (line.toLowerCase().indexOf(elt.text.toLowerCase()) != -1)) {
+	highlight.forEach(function (elt) {
+		if ((css == "") && (line.toLowerCase().indexOf(elt.text) != -1)) {
 			css = "alert alert-" + elt.level;
 		}
 	});
 	return css;
 }
 
-
-logfiles.forEach(function(log, index) {
-	log.css = '';
-	log.id = index;
-
-	tail[log.id] = new Tail(log.url);
-
-	tail[log.id].on('error', function(){
-	    console.log("ERROR FATAL - File " + log.url + " not found ");
-	    throw new Error("ERROR FATAL - File " + log.url + " not found ");
-	});
-
-
-	app.get('/file/' + log.id, function(req, res) {
-		var stat = fs.statSync(log.url);
-		res.writeHead(200, {
-			  'Content-Type': 'application/octet-stream', 
-		      'Content-Length': stat.size,
-		      'Content-disposition' : 'attachment; filename="' + log.name + '.log"'
-		});
-		var stream = fs.createReadStream(log.url, { bufferSize: 64 * 1024 });
-		stream.pipe(res);
-	});
-
-	io.of('/' + log.id).on('connection', function(client){
-
-		client.on('init', function(data) {
-
-			console.log('Initialization');
-			
-			fs.readFile(log.url, 'utf-8', function(err, data) {
-			    if (err) throw err;
-
-			    var lines = data.trim().split('\n');
-			    var lastLines = lines.slice(-300);
-
-			    lastLines.forEach( function(line) {
-			    	var css = getCss(line);
-			    	client.emit('Log', {
-			    		'text': line,
-			    		'css': css
-			    	});
-			    });
+io.of('/view').on('connection', function (client) {
+	client.on('init', function (relativePath) {
+		var filename = getPath(relativePath);
+		if (filename === null) {
+			client.emit('Log', {
+				text: 'Cannot find file: ' + relativePath
 			});
+			return;
+		}
 
+		if (fs.lstatSync(filename).isDirectory()) {
+			client.emit('Log', {
+				text: 'Directory selected'
+			})
+			return;
+		}
+
+		if (!tail[filename]) {
+			tail[filename] = new Tail(filename);
+			tail[filename].on('error', function () {
+				console.log("ERROR FATAL - File " + filename + " not found ");
+				throw new Error("ERROR FATAL - File " + filename + " not found ");
+			});
+		}
+
+		fs.readFile(filename, 'utf-8', function (err, data) {
+			if (err) throw err;
+
+			var lines = data.trim().split('\n');
+			var lastLines = lines.slice(-300);
+
+			lastLines.forEach(function (line) {
+				var css = getCss(line);
+				client.emit('Log', {
+					'text': line,
+					'css': css
+				});
+			});
 		});
 
-	    tail[log.id].on("line", function(data) {
-	    	var css = getCss(line);
-	    	client.emit('Log', {
-	    		'text': line,
-	    		'css': css
-	    	});
-	    });
-
+		tail[filename].on("line", function (data) {
+			var css = getCss(line);
+			client.emit('Log', {
+				'text': line,
+				'css': css
+			});
+		});
 	});
+});
 
+app.get('/download/:path', function (req, res) {
+	var filename = getPath(req.params.path);
+	var stat = fs.statSync(filename);
+	res.writeHead(200, {
+		'Content-Type': 'application/octet-stream',
+		'Content-Length': stat.size,
+		'Content-disposition': 'attachment; filename="' + path.basename(filename) + '.log"'
+	});
+	var stream = fs.createReadStream(filename, { bufferSize: 64 * 1024 });
+	stream.pipe(res);
 });
